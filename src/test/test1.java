15bec4a7e733d155dd2927867e96ee3f6629aae7
Ford <Chenji_yy@163.com>
2023-08-22 10:04:50 +0800

    first commit

--- src/test/test1.java
+++ src/test/test1.java
@@ -2,7 +2,7 @@ package test;
 
 public class test1 {
     public static void main(String[] args) {
-        System.out.printf("hello world!");
+        System.out.println("hello world!");
     }
 
 

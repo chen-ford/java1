eea782c18eda403ac2b08ccb04660f8f7fbd7ef0
Ford <Chenji_yy@163.com>
2023-08-22 10:20:01 +0800

    fourth commit

--- src/test/test1.java
+++ src/test/test1.java
@@ -3,7 +3,9 @@ package test;
 public class test1 {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("hello 1");
         System.out.println("hello 2");
+        System.out.println("hello 3");
     }
 
 
